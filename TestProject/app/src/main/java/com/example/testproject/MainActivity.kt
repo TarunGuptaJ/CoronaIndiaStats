b9b2ba366c5acb179634e259262558b2aeb800b4
KulkarniKaustubh <kmkapril15@gmail.com>
2020-06-01 15:45:36 +0530

    Renamed Welcome to State

--- TestProject/app/src/main/java/com/example/testproject/MainActivity.kt
+++ TestProject/app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,26 +1,11 @@
 package com.example.testproject
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.LocationManager
-import android.location.LocationListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.github.clans.fab.FloatingActionMenu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,11 +17,11 @@ class MainActivity : AppCompatActivity() {
         val floatingMenu : FloatingActionMenu = findViewById(R.id.menu)
 
         val indiaFAB : com.github.clans.fab.FloatingActionButton = findViewById(R.id.indiaFAB)
-        val welcomeFrag : Fragment = WelcomeFragment()
+        val stateFrag : Fragment = StateFragment()
         val indiaFrag : Fragment = IndiaFragment()
 
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, welcomeFrag)
+            replace(R.id.flFragment, indiaFrag)
             commit()
         }
 
@@ -51,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         stateFAB.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
                 floatingMenu.close(true)
-                replace(R.id.flFragment, welcomeFrag)
+                replace(R.id.flFragment, stateFrag)
                 commit()
             }
         }
---
+++ TestProject/app/src/main/java/com/example/testproject/StateFragment.kt
@@ -0,0 +1,7 @@
+package com.example.testproject
+
+import androidx.fragment.app.Fragment
+
+class StateFragment : Fragment(R.layout.state_fragment) {
+
+}
