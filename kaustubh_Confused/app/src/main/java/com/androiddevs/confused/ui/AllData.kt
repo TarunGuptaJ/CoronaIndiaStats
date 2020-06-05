c8e1ee4194af204594a9ed5ea69881688b2f8287
KulkarniKaustubh <kmkapril15@gmail.com>
2020-06-06 00:15:24 +0530

    Added required POKO classes for indiafragment.kt

---
+++ kaustubh_Confused/app/src/main/java/com/androiddevs/confused/ui/AllData.kt
@@ -0,0 +1,12 @@
+package com.androiddevs.confused.ui
+
+import com.google.gson.annotations.SerializedName
+
+data class AllData (
+
+	@SerializedName("activeCases") val active : Int,
+	@SerializedName("recovered") val recovered : Int,
+	@SerializedName("deaths") val deaths : Int,
+	@SerializedName("totalCases") val confirmed : Int,
+	@SerializedName("regionData") val regionData : List<RegionData>
+)
