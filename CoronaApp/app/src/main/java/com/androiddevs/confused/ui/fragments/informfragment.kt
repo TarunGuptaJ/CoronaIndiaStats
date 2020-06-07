f9c2211fb9790a59efe590029a478fc88affc69f
Tarun Gupta J <tjguptat1rs@gmail.com>
2020-06-07 23:19:37 +0530

    Removing redundancies/Load colour change

--- CoronaApp/app/src/main/java/com/androiddevs/confused/ui/fragments/informfragment.kt
+++
@@ -1,8 +0,0 @@
-package com.androiddevs.confused.ui.fragments
-
-import androidx.fragment.app.Fragment
-import com.androiddevs.confused.R
-
-class informfragment : Fragment(R.layout.inform_fragments){
-
-}
--- CoronaApp/app/src/main/java/com/androiddevs/confused/ui/stateApi.kt
+++
@@ -1,9 +0,0 @@
-package com.androiddevs.confused.ui
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-public interface stateApi {
-    @GET("v2/state_district_wise.json")
-    fun getState() : Call<List<State>>
-}
